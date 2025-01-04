bc980f0d002db82b91d6025feb52acca24474202
Appuffyt <lisaexe6@gmail.com>
2025-01-04 15:23:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["WATCHING PIXELHUB", "🎮 Playing VALORANT"];
+const statusTypes = [ 'dnd', 'AJINPS:)'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
