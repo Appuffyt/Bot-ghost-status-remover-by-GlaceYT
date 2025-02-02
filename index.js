d055c593b51d82b7de355ede3112a5e70be9a234
Appuffyt <lisaexe6@gmail.com>
2025-02-02 20:30:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,10 +37,55 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["WATCHING PIXEL HUB", "AJINPS:)"];
-const statusTypes = [ 'dnd', 'idle'];
-let currentStatusIndex = 0;
-let currentTypeIndex = 0;
+const { ActivityType } = require('discord.js');
+
+module.exports = {
+    name: 'ready',
+    once: true,
+    execute(client) {
+  
+        const activities = [
+            { name: 'Netflix', type: ActivityType.Watching },
+            { name: 'GTA VI', type: ActivityType.Playing },
+            { name: 'on YouTube', type: ActivityType.Streaming },
+            { name: 'Spotify', type: ActivityType.Listening },
+        ];
+
+     
+        const statuses = ['online', 'idle', 'dnd'];
+
+     
+        let currentActivityIndex = 0;
+        let currentStatusIndex = 0;
+
+ 
+        function setActivityAndStatus() {
+        
+            const activity = activities[currentActivityIndex];
+            const status = statuses[currentStatusIndex];
+
+          
+            client.user.setPresence({
+                activities: [activity],
+                status: status,
+            });
+
+            
+            currentActivityIndex = (currentActivityIndex + 1) % activities.length;
+            currentStatusIndex = (currentStatusIndex + 1) % statuses.length;
+        }
+
+        
+        setTimeout(() => {
+            setActivityAndStatus();
+            console.log('\x1b[31m[ CORE ]\x1b[0m \x1b[32m%s\x1b[0m', 'Bot Activity Set Successful ✅');
+        }, 2000);
+
+        setInterval(() => {
+            setActivityAndStatus();
+        }, 6000);
+    },
+};
 
 async function login() {
   try {
